66a3c092f2c59e3e34a559489435e2de6106742b
tianlang.zhou <tianlang.zhou@ucarinc.com>
2018-09-30 17:32:30 +0800

    Initial commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+
+  <template/>
+
+</template>
+
+<script>
+  import template from '../public/template.vue'
+  export default {
+    components:{
+      template
+    }
+  }
+</script>
+
+<style scoped>
+
+
+</style>
---
+++ src/pages/info/info.vue
@@ -0,0 +1,14 @@
+<template>
+  <span>
+    info
+  </span>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style>
+
+
+</style>
